aaf06eff8c24c73c8f426fc2f6a904ada0395a56
Ed Muzio <ed.muzio@n4c.com>
2024-06-05 11:44:16 +0100

    feat: Simple error handling
    Implemented try/catch block

--- src/functions/trigger_test.ts
+++ src/functions/trigger_test.ts
@@ -1,11 +1,16 @@
 import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/functions";
 
 export async function trigger_test(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
-    context.log(`Http function processed request for url "${request.url}"`);
+    try{    
+        context.log(`Http function processed request for url "${request.url}"`);
 
-    const name: string = request.query.get('name') || await request.text() || 'world';
+        const name: string = request.query.get('name') || await request.text() || 'world';
 
-    return { body: `Hello, ${name}!` };
+        return { body: `Hello, ${name}!` };
+    } catch (error) {
+        context.log(`Error handling request: ${error}`);
+        return { status: 500, body: 'Internal server error' };
+    }
 };
 
 app.http('trigger_test', {
@@ -13,3 +18,4 @@ app.http('trigger_test', {
     authLevel: 'anonymous',
     handler: trigger_test
 });
+
