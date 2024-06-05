56e023a7f7c103d67a09d2c7b37d543138ac49ed
Ed Muzio <ed.muzio@n4c.com>
2024-06-05 11:50:24 +0100

    style: impove readability

--- src/functions/trigger_test.ts
+++ src/functions/trigger_test.ts
@@ -3,11 +3,10 @@ import { app, HttpRequest, HttpResponseInit, InvocationContext } from "@azure/fu
 export async function trigger_test(request: HttpRequest, context: InvocationContext): Promise<HttpResponseInit> {
     try{    
         context.log(`Http function processed request for url "${request.url}"`);
-
         const name: string = request.query.get('name') || await request.text() || 'world';
-
         return { body: `Hello, ${name}!` };
-    } catch (error) {
+    } 
+    #catch (error) {
         context.log(`Error handling request: ${error}`);
         return { status: 500, body: 'Internal server error' };
     }
